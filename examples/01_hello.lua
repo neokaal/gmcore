df9b714d7bf53b754afea34a927b9e76c94bd8d5
Abhishek Mishra <abhishekmishra3@gmail.com>
2026-02-06 09:38:25 +0530

    show a single pixel in the center

--- examples/01_hello.lua
+++ examples/01_hello.lua
@@ -1,14 +1,6 @@
-print("hello")
-
 function frame(t)
     local w = width()
     local h = height()
 
-    for x = 0, w - 1 do
-        for y = 0, h - 1 do
-            if x == y then
-                px(x, y, 255, 255, 0)
-            end
-        end
-    end
+    px(w/2, h/2, 255, 0, 0)
 end
